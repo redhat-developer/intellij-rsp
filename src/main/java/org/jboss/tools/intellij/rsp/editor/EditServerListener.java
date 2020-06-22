5a3f4054077d87227aafa641efa3d5ec59bbd697
Rob Stryker <rob@oxbeef.net>
2020-06-22 15:45:43 -0400

    Initial impl for edit server, not working
    Signed-off-by: Rob Stryker <rob@oxbeef.net>

---
+++ src/main/java/org/jboss/tools/intellij/rsp/editor/EditServerListener.java
@@ -0,0 +1,126 @@
+package org.jboss.tools.intellij.rsp.editor;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.FileDocumentSynchronizationVetoer;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jboss.tools.intellij.rsp.client.IntelliJRspClientLauncher;
+import org.jboss.tools.intellij.rsp.model.IRsp;
+import org.jboss.tools.intellij.rsp.model.IRspType;
+import org.jboss.tools.intellij.rsp.model.impl.RspCore;
+import org.jboss.tools.intellij.rsp.ui.util.UIHelper;
+import org.jboss.tools.rsp.api.dao.ServerState;
+import org.jboss.tools.rsp.api.dao.Status;
+import org.jboss.tools.rsp.api.dao.UpdateServerRequest;
+import org.jboss.tools.rsp.api.dao.UpdateServerResponse;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.ExecutionException;
+
+public class EditServerListener extends FileDocumentSynchronizationVetoer {
+    public static final String NOTIFICATION_ID = "RSP Server";
+    public static final String EDITOR_KEY_RSP_ID = "rsp.editor.rsp.id";
+    public static final String EDITOR_KEY_SERVER_ID = "rsp.editor.server.id";
+    public static final Key<String> KEY_RSP_ID = Key.create(EditServerListener.EDITOR_KEY_RSP_ID);
+    public static final Key<String> KEY_SERVER_ID = Key.create(EditServerListener.EDITOR_KEY_SERVER_ID);
+
+
+    @Override
+    public boolean maySaveDocument(@NotNull Document document, boolean isSaveExplicit) {
+        VirtualFile vf = FileDocumentManager.getInstance().getFile(document);
+        String rspId = vf.getUserData(KEY_RSP_ID);
+        String serverId = vf.getUserData(KEY_SERVER_ID);
+
+        if(!isFileToPush(document, vf)) {
+            return true;
+        }
+
+        IRsp rspObj = RspCore.getDefault().findRsp(rspId);
+        ServerState ss = null;
+        String error = null;
+        if( rspObj == null ) {
+            error = "RSP " + rspId + " not found.";
+        } else {
+            ss = RspCore.getDefault().findServerInRsp(rspObj, serverId);
+            if( ss == null ) {
+                error = "Server " + ss.getServer().getId() + " not found in RSP " + rspId;
+            }
+        }
+
+        if( error != null ) {
+            Notification notification = new Notification(NOTIFICATION_ID, "Error",
+                    "An error occurred: " + error, NotificationType.ERROR);
+            Notifications.Bus.notify(notification);
+            return false;
+        }
+
+        int resultDialog = UIHelper.executeInUI(() ->
+                Messages.showYesNoDialog(
+                        "Do you want to push the changes to the RSP?",
+                        "Save to RSP",
+                        null
+                ));
+
+        if (resultDialog != Messages.OK) return false;
+
+
+        IntelliJRspClientLauncher client = rspObj.getModel().getClient(rspObj);
+        UpdateServerResponse saveResult = null;
+        try {
+            UpdateServerRequest request = new UpdateServerRequest();
+            request.setHandle(ss.getServer());
+            request.setServerJson(document.getText());
+            saveResult = client.getServerProxy().updateServer(null).get();
+        } catch (InterruptedException | ExecutionException e) {
+            Notification notification = new Notification(NOTIFICATION_ID, "Error",
+                    "An error occurred while saving text to RSP: " + e.getMessage(), NotificationType.ERROR);
+            Notifications.Bus.notify(notification);
+            return false;
+        }
+
+        if( saveResult == null ) {
+            Notification notification = new Notification(NOTIFICATION_ID, "Error",
+                    "An error occurred while saving text to RSP: Null response from server", NotificationType.ERROR);
+            Notifications.Bus.notify(notification);
+            return false;
+        }
+
+        if( !saveResult.getValidation().getStatus().isOK()) {
+            Status stat = saveResult.getValidation().getStatus();
+            document.setText(saveResult.getServerJson().getServerJson());
+            Notification notification = new Notification(NOTIFICATION_ID, "Error",
+                    "An error occurred while saving text to RSP: " + stat.getMessage(), NotificationType.ERROR);
+            Notifications.Bus.notify(notification);
+            return false;
+        }
+
+        document.setText(saveResult.getServerJson().getServerJson());
+        // notify user if saving was completed successfully
+        Notification notification = new Notification(NOTIFICATION_ID, "Save Successful",
+                "Server " + ss.getServer().getId() + " saved successfully.", NotificationType.INFORMATION);
+        Notifications.Bus.notify(notification);
+        return false;
+    }
+
+    private boolean isFileToPush(Document document, VirtualFile vf) {
+        Project project = ProjectManager.getInstance().getOpenProjects()[0];
+        Editor selectedEditor = FileEditorManager.getInstance(project).getSelectedTextEditor();
+        // if file is not the one selected, skip it
+        if (selectedEditor == null || selectedEditor.getDocument() != document) return false;
+        // if file is not related to rsp, skip it
+        if (vf == null || vf.getUserData(KEY_RSP_ID) == null || vf.getUserData(KEY_SERVER_ID) == null) {
+            return false;
+        }
+        return true;
+    }
+}
