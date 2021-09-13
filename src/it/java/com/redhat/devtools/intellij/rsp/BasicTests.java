d456dafb155201e9ab2a7079ccfa4319c61b7838
Oleksii Korniienko <olkornii@redhat.com>
2021-09-13 12:53:20 +0200

    Reworked itest for check if connectors exists
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- src/it/java/com/redhat/devtools/intellij/rsp/BasicTests.java
+++ src/it/java/com/redhat/devtools/intellij/rsp/BasicTests.java
@@ -19,16 +19,23 @@ import java.util.List;
 import com.intellij.remoterobot.fixtures.ComponentFixture;
 import com.intellij.remoterobot.fixtures.dataExtractor.RemoteText;
 import com.intellij.remoterobot.utils.WaitForConditionTimeoutException;
-import com.redhat.devtools.intellij.rsp.dialogs.NewProjectDialogFixture;
-import com.redhat.devtools.intellij.rsp.dialogs.WelcomeFrameDialogFixture;
+import com.redhat.devtools.intellij.rsp.dialogs.ProjectStructureDialog;
 import com.redhat.devtools.intellij.rsp.mainIdeWindow.RspToolFixture;
-import com.redhat.devtools.intellij.rsp.mainIdeWindow.ToolWindowsPaneFixture;
-import com.redhat.devtools.intellij.rsp.utils.GlobalUtils;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import com.redhat.devtools.intellij.commonUiTestLibrary.UITestRunner;
+import com.redhat.devtools.intellij.commonUiTestLibrary.fixtures.dialogs.FlatWelcomeFrame;
+import com.redhat.devtools.intellij.commonUiTestLibrary.fixtures.dialogs.projectManipulation.NewProjectDialog;
+import com.redhat.devtools.intellij.commonUiTestLibrary.fixtures.mainIdeWindow.toolWindowsPane.ToolWindowsPane;
+import com.redhat.devtools.intellij.commonUiTestLibrary.fixtures.dialogs.information.TipDialog;
+import com.redhat.devtools.intellij.commonUiTestLibrary.fixtures.mainIdeWindow.ideStatusBar.IdeStatusBar;
+
+import com.redhat.devtools.intellij.rsp.tests.checkRspConnectorsExists;
+
+import static com.intellij.remoterobot.stepsProcessing.StepWorkerKt.step;
 import static com.intellij.remoterobot.utils.RepeatUtilsKt.waitFor;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * JUnit UI tests for intellij-rsp
@@ -41,56 +48,53 @@ public class BasicTests {
     private static ComponentFixture rspViewTree;
 
     @BeforeAll
-    public static void connect() throws InterruptedException {
-        GlobalUtils.waitUntilIntelliJStarts(8082);
-        robot = GlobalUtils.getRemoteRobotConnection(8082);
-        GlobalUtils.clearTheWorkspace(robot);
+    public static void connect() {
+        robot = UITestRunner.runIde(UITestRunner.IdeaVersion.V_2020_3, 8580);
         createEmptyProject();
-    }
-
-    @Test
-    public void checkRspServersExists() {
-        final ToolWindowsPaneFixture toolWindowsPaneFixture = robot.find(ToolWindowsPaneFixture.class);
-        waitFor(Duration.ofSeconds(10), Duration.ofSeconds(1), "The 'Kubernetes' stripe button is not available.", () -> isStripeButtonAvailable(toolWindowsPaneFixture, "RSP Servers"));
-        toolWindowsPaneFixture.stripeButton("RSP Servers").click();
+        final ToolWindowsPane toolWindowsPane = robot.find(ToolWindowsPane.class);
+        waitFor(Duration.ofSeconds(15), Duration.ofSeconds(1), "The 'RSP servers' stripe button is not available.", () -> isStripeButtonAvailable(toolWindowsPane, "RSP Servers"));
+        toolWindowsPane.stripeButton("RSP Servers").click();
 
         RspToolFixture rspToolFixture = robot.find(RspToolFixture.class);
         rspViewTree = rspToolFixture.getRspViewTree();
-        waitFor(Duration.ofSeconds(15), Duration.ofSeconds(1), "Kubernetes Tree View is not available.", BasicTests::isRspViewTreeAvailable);
-
-        List<RemoteText> treeElements = rspViewTree.findAllText();
-        assertTrue(treeElements.size() == 2);
-
-        String firstPosition = treeElements.get(0).getText();
-        assertTrue(firstPosition.contains("Community Server Connector by Red Hat"));
-        String secondPosition = treeElements.get(1).getText();
-        assertTrue(secondPosition.contains("Red Hat Server Connector"));
+        waitFor(Duration.ofSeconds(15), Duration.ofSeconds(1), "RSP Tree View is not available.", () -> isRspViewTreeAvailable(rspViewTree));
     }
 
+    @AfterAll
+    public static void closeIde() {
+        UITestRunner.closeIde();
+    }
+
+    @Test
+    public void checkRspConnectorsExists() {
+        step("New Empty Project", () -> checkRspConnectorsExists.checkRspConnectors(rspViewTree));
+    }
 
     private static void createEmptyProject(){
-        final WelcomeFrameDialogFixture welcomeFrameDialogFixture = robot.find(WelcomeFrameDialogFixture.class);
-        welcomeFrameDialogFixture.createNewProjectLink().click();
-        final NewProjectDialogFixture newProjectDialogFixture = welcomeFrameDialogFixture.find(NewProjectDialogFixture.class, Duration.ofSeconds(20));
-        newProjectDialogFixture.projectTypeJBList().findText("Empty Project").click();
-        newProjectDialogFixture.button("Next").click();
-        newProjectDialogFixture.button("Finish").click();
-        GlobalUtils.waitUntilTheProjectImportIsComplete(robot);
-        GlobalUtils.cancelProjectStructureDialogIfItAppears(robot);
-        GlobalUtils.closeTheTipOfTheDayDialogIfItAppears(robot);
-        GlobalUtils.waitUntilAllTheBgTasksFinish(robot);
+        final FlatWelcomeFrame flatWelcomeFrame = robot.find(FlatWelcomeFrame.class);
+        flatWelcomeFrame.createNewProject();
+        final NewProjectDialog newProjectDialogFixture = flatWelcomeFrame.find(NewProjectDialog.class, Duration.ofSeconds(20));
+        newProjectDialogFixture.selectNewProjectType("Empty Project");
+        newProjectDialogFixture.next();
+        newProjectDialogFixture.finish();
+
+        final IdeStatusBar ideStatusBar = robot.find(IdeStatusBar.class);
+        ideStatusBar.waitUntilProjectImportIsComplete();
+        TipDialog.closeTipDialogIfItAppears(robot);
+        ProjectStructureDialog.cancelProjectStructureDialogIfItAppears(robot);
+        ideStatusBar.waitUntilAllBgTasksFinish();
     }
 
-    private static boolean isStripeButtonAvailable(ToolWindowsPaneFixture toolWindowsPaneFixture, String label) {
+    private static boolean isStripeButtonAvailable(ToolWindowsPane toolWindowsPane, String label) {
         try {
-            toolWindowsPaneFixture.stripeButton(label);
+            toolWindowsPane.stripeButton(label);
         } catch (WaitForConditionTimeoutException e) {
             return false;
         }
         return true;
     }
 
-    private static boolean isRspViewTreeAvailable(){
+    private static boolean isRspViewTreeAvailable(ComponentFixture rspViewTree){
         List<RemoteText> allText = rspViewTree.findAllText();
         String firstText = allText.get(0).getText();
         return !"Nothing to show".equals(firstText);
