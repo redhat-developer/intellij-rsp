04ff032c525466586686fb61832608ac72ecc391
Oleksii Korniienko <olkornii@redhat.com>
2021-09-13 12:53:20 +0200

    Reworked itest for check if connectors exists
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

---
+++ src/it/java/com/redhat/devtools/intellij/rsp/tests/checkRspConnectorsExists.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.rsp.tests;
+
+import com.intellij.remoterobot.fixtures.ComponentFixture;
+import com.intellij.remoterobot.fixtures.dataExtractor.RemoteText;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author olkornii@redhat.com
+ */
+public class checkRspConnectorsExists extends AbstractRspServersTest{
+    public static void checkRspConnectors(ComponentFixture rspViewTree){
+        List<RemoteText> treeElements = rspViewTree.findAllText();
+        assertTrue(treeElements.size() == 2);
+
+        String firstPosition = treeElements.get(0).getText();
+        assertTrue(firstPosition.contains("Community Server Connector by Red Hat"));
+        String secondPosition = treeElements.get(1).getText();
+        assertTrue(secondPosition.contains("Red Hat Server Connector"));
+    }
+}
