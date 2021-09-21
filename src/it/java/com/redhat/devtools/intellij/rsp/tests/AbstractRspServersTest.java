aee0e9a9a4bc7cfad817bd890ffeb654c6b88222
Oleksii Korniienko <olkornii@redhat.com>
2021-09-21 10:24:07 +0200

    Added runRspConnectors test
    Signed-off-by: Oleksii Korniienko <olkornii@redhat.com>

--- src/it/java/com/redhat/devtools/intellij/rsp/tests/AbstractRspServersTest.java
+++ src/it/java/com/redhat/devtools/intellij/rsp/tests/AbstractRspServersTest.java
@@ -13,21 +13,12 @@ package com.redhat.devtools.intellij.rsp.tests;
 import com.intellij.remoterobot.fixtures.ComponentFixture;
 import com.intellij.remoterobot.fixtures.dataExtractor.RemoteText;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 /**
  * @author olkornii@redhat.com
  */
-public class CheckRspConnectorsExists {
-    public static void checkRspConnectors(ComponentFixture rspViewTree){
-        List<RemoteText> treeElements = rspViewTree.findAllText();
-        assertTrue(treeElements.size() == 2);
-
-        String firstPosition = treeElements.get(0).getText();
-        assertTrue(firstPosition.contains("Community Server Connector by Red Hat"));
-        String secondPosition = treeElements.get(1).getText();
-        assertTrue(secondPosition.contains("Red Hat Server Connector"));
+public abstract class AbstractRspServersTest {
+    public static boolean isRspServerStarted(ComponentFixture rspViewTree, int serverNumber){
+        RemoteText server = rspViewTree.findAllText().get(serverNumber);
+        return server.getText().contains("[STARTED]");
     }
 }
---
+++ src/it/java/com/redhat/devtools/intellij/rsp/tests/RunRspConnectorsTest.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.rsp.tests;
+
+import com.intellij.remoterobot.RemoteRobot;
+import com.intellij.remoterobot.fixtures.ComponentFixture;
+import com.intellij.remoterobot.fixtures.JPopupMenuFixture;
+import com.redhat.devtools.intellij.commonUiTestLibrary.fixtures.mainIdeWindow.ideStatusBar.IdeStatusBar;
+import org.assertj.swing.core.MouseButton;
+
+import java.time.Duration;
+
+import static com.intellij.remoterobot.utils.RepeatUtilsKt.waitFor;
+
+/**
+ * @author olkornii@redhat.com
+ */
+public class RunRspConnectorsTest extends AbstractRspServersTest {
+    public static void runRspServers(RemoteRobot robot, ComponentFixture rspViewTree){
+        int treeElementsCount = rspViewTree.findAllText().size();
+
+        for (int i = 0; i < treeElementsCount; i++){
+            int serverNumber = i;
+            rspViewTree.findAllText().get(serverNumber).click(MouseButton.RIGHT_BUTTON);
+            JPopupMenuFixture contextMenu = robot.find(JPopupMenuFixture.class, JPopupMenuFixture.Companion.byType(), Duration.ofSeconds(10));
+            contextMenu.select("Download / Update RSP");
+            final IdeStatusBar ideStatusBar = robot.find(IdeStatusBar.class);
+            ideStatusBar.waitUntilAllBgTasksFinish();
+            rspViewTree.findAllText().get(serverNumber).click(MouseButton.RIGHT_BUTTON);
+            contextMenu = robot.find(JPopupMenuFixture.class, JPopupMenuFixture.Companion.byType(), Duration.ofSeconds(10));
+            contextMenu.select("Start RSP");
+            waitFor(Duration.ofSeconds(15), Duration.ofSeconds(1), "Server did not started.", () -> isRspServerStarted(rspViewTree ,serverNumber));
+        }
+    }
+}
